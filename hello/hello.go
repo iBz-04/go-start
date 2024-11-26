684937550fb6320457bf6f569964722a316d826f
iBz-04 <issakaibrahimrayamah@gmail.com>
2024-11-27 01:52:13 +0300

    multiple name instance

--- hello/hello.go
+++ hello/hello.go
@@ -11,8 +11,12 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
+    // slice of names 
+    names := []string{"Ibrah", "fauzan", "Sidik"}
+
+
     // request a greeting message.
-    message, err := greetings.Hello("Ibrah")
+    messages, err := greetings.Hellos(names)
 
 
     if err != nil {
@@ -20,5 +24,5 @@ func main() {
     }
 
     // Get a greeting message and print it.
-    fmt.Println(message)
+    fmt.Println(messages)
 }
